fbc540b89441172005a4e4c29fba47a2eb2cbbe6
Sudhish8103 <31006245+Sudhish8103@users.noreply.github.com>
2017-08-14 20:01:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 var img = document.getElementById('sudi');
+var marginLeft
+marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + "px";
+}
 img.onclick = function (){
    var interval = setInterval(moveRight, 1000);
     
