fb8554913b095a96b39533f8502b2a8958809e7c
Sudhish8103 <31006245+Sudhish8103@users.noreply.github.com>
2017-08-14 20:42:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,10 @@
-console.log('Loaded!');
-var img = document.getElementById('sudi');
-var marginLeft
-marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + "px";
-}
-img.onclick = function (){
-   var interval = setInterval(moveRight, 50);
+// Counter Code
+var counter = 0;
+var button = document.getElementById("counter");
+button.onclick = function() {
     
+  // Render the variable in the correct span
+  counter = counter + 1
+  var span = document.getElementById("count");
+  span.innerHTML = counter.toString();
 };
