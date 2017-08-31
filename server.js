73207d91650683b73c295506bf8c5f22caf51da4
Sudhish8103 <31006245+Sudhish8103@users.noreply.github.com>
2017-08-31 17:24:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'whyblue123',
@@ -53,6 +54,17 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+function hash (input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function (req, res) {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hasedString);
 });
 
 var pool = new Pool(config);
