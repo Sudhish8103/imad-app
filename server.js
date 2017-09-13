e7d6f124eec4d1f87dfa3cf83de48bf3240b04b1
Sudhish8103 <31006245+Sudhish8103@users.noreply.github.com>
2017-09-13 18:46:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
+const Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
-
+var config = {
+    user: 'whyblue123',
+    database: 'whyblue123',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    paswword: process.env.DB_PASSWORD
+};
 
 
 var app = express();
@@ -13,6 +19,19 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+const pool = new Pool();
+app.get('/test-db', function (req, res){
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM test', function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+     }
+   });
+});
+
 var counter = 0;
 app.get('/counter', function(req, res){
    counter = counter + 1;
