932d9b4606a283c2ccf3fa0196a9f4d793fc7a20
Sudhish8103 <31006245+Sudhish8103@users.noreply.github.com>
2017-09-13 18:49:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,60 +4,101 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+
 var config = {
     user: 'whyblue123',
     database: 'whyblue123',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    paswword: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD
 };
 
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+    
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool = new Pool();
-app.get('/test-db', function (req, res){
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT * FROM test', function (err, result) {
-        if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result));
-     }
-   });
-});
-
 var counter = 0;
 app.get('/counter', function(req, res){
    counter = counter + 1;
-   res.send(counter.toString());
+   res.send(counter.toString() );
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var names = [];
-app.get('/submit-name', function (req,res){// /submit-name?name=xxxx
-    var name = req.query.name;
-    names.push(name);
-    res.send(counter.toString());
-});
-
-app.get('/:articleName', function(req,res) { 
+app.get('/articles/:articleName', function(req,res) {
     // articleNmae == article-one
     // articles[articleName] == { } content object for article one
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
     
+    pool.query("SELECT * FROM article WHERE title = ' " + req.params.articleName + " ' ", function (err,result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        }
+        else {
+            if (result.length.rows === 0) {
+                res.status(404).send('Article not found');
+            }
+            else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+function hash (input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+app.get('/hash/:input', function (req, res) {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res){
+   // USERNAME, PWD
+   var username = req.body.username;
+   var password = req.body.password;
+    
+   var salt = getRandomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user"  (username, password) VALUES ($1, $2)' , [username, dbString], function (err,result){
+        if (err) {
+        res.status(500).send(err.toString());
+        }
+        else{
+            res.send('User successfully created:' + username);
+        }
+   }); 
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req,res) {
+// make a select request
+// return a response with the results
+pool.query('SELECT * FROM test', function (err,result) {
+    if (err) {
+        res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result.rows));
+        }
+   });
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
